63be4e698754a53921baf0273eb6084cb4b1fdba
Francis-Clarance <francis.clarance@version1.com>
2026-02-23 17:32:20 +0000

    Add VPC configuration with enhanced settings

--- DSIT-import/vpc.tf
+++ DSIT-import/vpc.tf
@@ -1,4 +1,11 @@
 resource "aws_vpc" "dsit-litellm-gateway-main" {
+  cidr_block                           = "10.0.0.0/16"
+  enable_dns_hostnames                 = true
+  enable_dns_support                   = true 
+  enable_network_address_usage_metrics = "false"
+  instance_tenancy                     = "default"
+  ipv6_netmask_length                  = "0"
+
   tags = {
     Name = "dsit-litellm-gateway-main"
   }
